4e9c0e7825aee35efe5c9274750032642595fe08
Adam Wenham <adamwenham64@gmail.com>
2013-04-26 07:18:24 +0100

    02 bonus halfway there

--- 02_calculator/calculator_spec.rb
+++ 02_calculator/calculator_spec.rb
@@ -79,14 +79,20 @@ end
 
 describe "#multiply" do
 
-  it "multiplies two numbers"
+  it "multiplies two numbers" do
+     multiply([2,3]).should == 6
+  end
 
-  it "multiplies several numbers"
-
+  it "multiplies several numbers" do
+     multiply([2,3,4]).should == 24
+  end
 end
 
+
 describe "#power" do
-  it "raises one number to the power of another number"
+  it "raises one number to the power of another number" do
+    power(2,4).should == 32
+  end
 end
 
 # http://en.wikipedia.org/wiki/Factorial
