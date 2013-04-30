cb883929db12d997535b9b077be17d12c5e8bc25
Adam Wenham <adamwenham64@gmail.com>
2013-04-30 07:04:59 +0100

    03 complete

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -6,8 +6,26 @@ def shout(string)
   string.upcase
 end
 
-def repeat(string, num=1)
-  num.times do |rep|
+def repeat(string, times = 2)
+  ([string] * times).join(" ")
+end
 
+def start_of_word(string,index)
+  string[0...index]
+end
+
+def first_word(string)
+  string.split(" ")[0]
+end
+
+def titleize(string)
+  sentence = string.split.map do |x|
+    if "the and over".include?(x)
+        x
+      else
+        x.capitalize
+    end
   end
+  sentence.first.capitalize!
+  sentence.join(" ")
 end
